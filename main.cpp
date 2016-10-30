3a9ebc4539de6326e79c21b1c6ee0d89dc82a2d1
Dorzhiq <Dorzhiq@gmail.com>
2016-10-31 00:35:33 +0700

    create project

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+
+double fun(double x, double y, double z){
+    return x+y+z;
+}
+
+double dif2(double x, double y, double z, double(*U)(double,double,double),
+            double h1,double h2, double h3){
+    return (U(x,y,z) + U(x+h1, y+h2, z+h3))/2;
+}
+
+int main(int argc, char *argv[])
+{
+    double W[10][10];
+    std::cout<<dif2(1,1,1,fun,0.1,0.05,0.01)<<std::endl;
+}
+
